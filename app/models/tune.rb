592bbc1f6f128097950ec8af748c000bca2cbd32
huro3h <huro3h@gmail.com>
2018-01-09 08:05:15 +0900

    Create crawl method that collect set list

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -1,2 +1,25 @@
 class Tune < ApplicationRecord
+  require 'net/http'
+  require 'uri'
+  require 'json'
+  require 'open-uri'
+
+  def self.crawl
+    uri = URI.parse('https://api.amebaowndme.com/v2/public/blogPosts/3484732')
+    json = Net::HTTP.get(uri)
+    result = JSON.parse(json)
+
+    # p result['body']['title']
+    list = result['body']['contents'][1]['value']
+    set_lists = list.split('<p>')
+    ary = []
+
+    set_lists.each do |set_list|
+      set_list.gsub!(/(<br>|<\/p>|&nbsp;)/, '')
+      set_list.gsub!(/\s{2,}/, ' ')
+      set_list.strip!
+      ary << set_list
+    end
+    p ary
+  end
 end
