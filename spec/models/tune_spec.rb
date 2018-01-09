3082a7ac55670b2fd62775b6fdfdc29fe1107b33
huro3h <huro3h@gmail.com>
2018-01-09 09:52:09 +0900

    rails g rspec:install

--- spec/models/tune_spec.rb
+++ spec/models/tune_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Tune, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Tune do
+  describe '#requested?' do
+    it '受け取った値の末尾が * で終わればリクエスト曲であること' do
+      set_list = 'The great sample tune *'
+      requested?(set_list).to eq("#{set_list}はリクエスト曲です！")
+    end
+  end
 end
+
+
