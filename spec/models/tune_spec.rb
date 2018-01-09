6f39fd0876de4de67a046c771f067903887b3272
huro3h <huro3h@gmail.com>
2018-01-10 08:03:36 +0900

    Fix Tune#requested? method test

--- spec/models/tune_spec.rb
+++ spec/models/tune_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 describe Tune do
   describe '#requested?' do
     it '受け取った値の末尾が * で終わればリクエスト曲であること' do
-      set_list = 'The great sample tune *'
-      requested?(set_list).to eq("#{set_list}はリクエスト曲です！")
+      tune = 'The great sample tune *'
+      expect(Tune.requested?(tune)).to eq("#{tune}はリクエスト曲です！")
     end
   end
 end
-
-
