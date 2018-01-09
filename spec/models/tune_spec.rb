c9b4d0e00bd92e7aad57c60d24c1b0ffee893fa1
huro3h <huro3h@gmail.com>
2018-01-10 08:24:50 +0900

    Update tune_spec.rb for Tune#list_format method

--- spec/models/tune_spec.rb
+++ spec/models/tune_spec.rb
@@ -7,4 +7,21 @@ describe Tune do
       expect(Tune.requested?(tune)).to eq("#{tune}はリクエスト曲です！")
     end
   end
+
+  describe '#list_format' do
+    it '値に<br>のタグ文字が含まれていれば取り除くこと' do
+      str = '<br>The great <br>sample tune'
+      expect(Tune.list_format(str)).to eq('The great sample tune')
+    end
+
+    it '2個以上連続する半角スペースを半角スペース1個に変換すること' do
+      str = 'The    great sample     tune'
+      expect(Tune.list_format(str)).to eq('The great sample tune')
+    end
+
+    it '"/"で区切られたセパレーターを"-"に変換すること' do
+      str = 'Special Artist / The great sample tune'
+      expect(Tune.list_format(str)).to eq('Special Artist - The great sample tune')
+    end
+  end
 end
