99a944dbc356f02e4a94dcb98c1216aa5194746b
huro3h <huro3h@gmail.com>
2018-01-12 09:01:00 +0900

    Changing method type class to instance

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -4,7 +4,7 @@ class Tune < ApplicationRecord
   require 'json'
   require 'open-uri'
 
-  def self.crawl
+  def crawl
     # p page = agent.get("https://groover-seo.amebaownd.com/posts/3484732")
     uri = URI.parse('https://api.amebaowndme.com/v2/public/blogPosts/3484732')
     json = Net::HTTP.get(uri)
@@ -12,18 +12,16 @@ class Tune < ApplicationRecord
 
     # p result['body']['title']
     list = result['body']['contents'][1]['value']
-    self.to_list(list)
+
+    to_list(list)
   end
 
-  # private
-
-  def self.to_list(list)
-    set_lists = list.split('<p>')
+  def to_list(list)
     ary = []
+    set_lists = list.split('<p>')
 
     set_lists.each do |set_list|
       self.list_format(set_list)
-
       next if set_list.blank?
       self.requested?(set_list)
       ary << set_list
@@ -31,15 +29,16 @@ class Tune < ApplicationRecord
     ary
   end
 
-  def self.list_format(set_list)
-    set_list.gsub!(/(<br>|<\/p>|&nbsp;)/, '')
+  def list_format(set_list)
+    set_list.gsub!(%r{<br>|<\/p>|&nbsp;}, '')
     set_list.gsub!(/\s{2,}/, ' ')
-    set_list.gsub!(/ \/ /, ' - ')
+    set_list.gsub!(%r{ \/ }, ' - ')
     set_list.strip!
     set_list
   end
 
-  def self.requested?(set_list)
+  def requested?(set_list)
     "#{set_list}はリクエスト曲です！" if set_list =~ /\*$/
   end
+
 end
