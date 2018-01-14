1ae19f57e6ba371449012e39c04d7983ae970ffd
huro3h <huro3h@gmail.com>
2018-01-15 00:50:08 +0900

    Update event model about look for set list uri

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,24 @@
 class Event < ApplicationRecord
+  # require 'net/http'
+  # require 'uri'
+  require 'nokogiri'
+  require 'open-uri'
 
   has_many :tunes
+
+  def self.t # search_set_list
+    url = 'https://groover-seo.amebaownd.com/rss.xml'
+    xml = Nokogiri::XML(open(url).read)
+    item_nodes = xml.xpath('//item')
+
+    item_nodes.each do |item|
+      title = item.xpath('title').text
+      link = item.xpath('link').text
+      if title =~ /「SETLIST」/
+        # TODO: titleとlinkをEventテーブルに放り込む
+      end
+    end
+  end
 
   def self.title_call(title)
     title unless title.nil?
