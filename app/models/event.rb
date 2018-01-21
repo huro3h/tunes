fc396aec574f515e9a588780afd6c3ffc549b346
huro3h <huro3h@gmail.com>
2018-01-22 08:34:04 +0900

    Update event model methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,23 +14,24 @@ class Event < ApplicationRecord
 
   def search_from_sources
     url = 'https://groover-seo.amebaownd.com/rss.xml'
+    ary = []
     return if url.nil?
     xml = Nokogiri::XML(open(url).read)
     item_nodes = xml.xpath('//item')
     item_nodes.each do |item|
       title = item.xpath('title').text
       link = item.xpath('link').text
-      # a_record(title, link) if title =~ /「SETLIST」/
+      ary << [title, link] if title =~ /「SETLIST」/
     end
+    record_events(ary)
   end
 
-  # def a_record(title, link)
-  #   @event = Event.new
-  #   @event.title = title
-  #   @event.url = link
-  # end
+  def record_events(events)
+    # p events
+  end
 
   def date_format_from_title(title)
-    title[0].to_s if title = title.match(/\d{4}.\d{1,2}.\d{1,2}/)
+    date_at = title[0].to_s if title = title.match(/\d{4}.\d{1,2}.\d{1,2}/)
+    DateTime.parse(date_at)
   end
 end
