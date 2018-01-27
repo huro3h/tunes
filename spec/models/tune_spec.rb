acd30282ace8d012ba92e5cb8695ff92f4e5e7e3
huro3h <huro3h@gmail.com>
2018-01-27 19:39:27 +0900

    Fix tune test using instance variable instead of let

--- spec/models/tune_spec.rb
+++ spec/models/tune_spec.rb
@@ -10,32 +10,31 @@ describe Tune do
   end
 
   describe '#list_format' do
-    before do
-      @tune = Tune.new
-    end
+    let(:tune) { Tune.new }
+
     it '値に改行を表すタグ文字が含まれていれば取り除くこと' do
       str = '<br>The great <br>sample tune'
-      expect(@tune.list_format(str)).to eq('The great sample tune')
+      expect(tune.list_format(str)).to eq('The great sample tune')
     end
 
     it '値に<p>の閉じタグが含まれていれば取り除くこと' do
       str = 'The great sample tune</p>'
-      expect(@tune.list_format(str)).to eq('The great sample tune')
+      expect(tune.list_format(str)).to eq('The great sample tune')
     end
 
     it '値に"&nbsp;"が含まれていれば取り除くこと' do
       str = 'The great &nbsp;sample tu&nbsp;ne'
-      expect(@tune.list_format(str)).to eq('The great sample tune')
+      expect(tune.list_format(str)).to eq('The great sample tune')
     end
 
     it '2個以上連続する半角スペースを半角スペース1個に変換すること' do
       str = 'The    great sample     tune'
-      expect(@tune.list_format(str)).to eq('The great sample tune')
+      expect(tune.list_format(str)).to eq('The great sample tune')
     end
 
     it '"/"で区切られたセパレーターを"-"に変換すること' do
       str = 'Special Artist / The great sample tune'
-      expect(@tune.list_format(str)).to eq('Special Artist - The great sample tune')
+      expect(tune.list_format(str)).to eq('Special Artist - The great sample tune')
     end
   end
 end
